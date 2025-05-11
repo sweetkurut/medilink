27e308417212a540f2006248a6dce83ff305f4cd
sweetkurut <devxwc@gmail.com>
2025-05-12 01:46:04 +0600

    add login page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,16 +2,23 @@ import { Header } from "@/widgets/Header/Header";
 import { AppRouter } from "./providers/router";
 import "./styles/index.scss";
 import { Footer } from "@/widgets/Footer/Footer";
+import { useLocation } from "react-router-dom";
+import { routeConfig } from "@/shared/config/routeConfig/routeConfig";
 
 function App() {
+    const location = useLocation();
+
+    // const isAuthPage = location.pathname === routeConfig.login || location.pathname === routeConfig.signup;
+    const isAuthPage = location.pathname === routeConfig.login.path || location.pathname === routeConfig.signup.path;
+
     return (
         <>
             <div className={"app"}>
-                <Header />
+                {!isAuthPage && <Header />}
                 <main className="content-page">
                     <AppRouter />
                 </main>
-                <Footer />
+                {!isAuthPage && <Footer />}
             </div>
         </>
     );
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,45 @@
 import s from "./login.module.scss";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Checkbox, Form, Input, Flex } from "antd";
+import { useNavigate } from "react-router-dom";
 
 export const LoginPage = () => {
-    return <div className={s.wrapper}>LoginPage</div>;
+    const nav = useNavigate();
+    const handleNavigate = () => {
+        nav("/src/pages/MainPage/MainPage.tsx");
+    };
+
+    const onFinish = (values: string) => {
+        console.log("Received values of form: ", values);
+    };
+
+    return (
+        <div className={s.wrapper}>
+            <div className={s.container}>
+                <Form name="login" initialValues={{ remember: true }} style={{ maxWidth: 400 }} onFinish={onFinish} className={s.form}>
+                    <Form.Item name="username" rules={[{ required: true, message: "Укажите имя" }]}>
+                        <Input prefix={<UserOutlined />} placeholder="Имя" />
+                    </Form.Item>
+                    <Form.Item name="password" rules={[{ required: true, message: "Укажите пароль" }]}>
+                        <Input.Password prefix={<LockOutlined />} type="password" placeholder="Пароль" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Flex justify="space-between" align="center">
+                            <Form.Item name="remember" valuePropName="checked" noStyle>
+                                <Checkbox>Запомнить меня</Checkbox>
+                            </Form.Item>
+                            <a href="#">Забыли пароль</a>
+                        </Flex>
+                    </Form.Item>
+
+                    <Form.Item>
+                        <Button block type="primary" htmlType="submit" onClick={handleNavigate}>
+                            Войти
+                        </Button>
+                        Нету аккаунта? <a href="">Зарегистрироваться</a>
+                    </Form.Item>
+                </Form>
+            </div>
+        </div>
+    );
 };
