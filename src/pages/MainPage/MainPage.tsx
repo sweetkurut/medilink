926ea06db59d34807d118e8508156a24ebdb311b
sweetkurut <devxwc@gmail.com>
2025-05-17 13:42:49 +0600

    fix sidebar

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -3,7 +3,7 @@ import s from "./mainpage.module.scss";
 export const MainPage = () => {
     return (
         <div className={s.wrapper}>
-            <div className={s.container}>MainPage</div>
+            <div className={s.container}>Главная страница</div>
         </div>
     );
 };
